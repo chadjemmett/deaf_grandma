88327a51e65183253a0bb1d2b24000394f2f9ec9
ceejaay <chad.jemmett@gmail.com>
2012-05-01 20:38:02 -0600

    The final deaf grandma program.

---
+++ deaf_grandma6.rb
@@ -0,0 +1,40 @@
+#this works as of March 16, 2012
+#the following method was made for ease of reding in the terminal window.
+#deaf grandma 6
+#def spacing
+ #puts ''
+ #puts ''
+ #puts ''
+ #puts ''
+#end
+
+count = 0
+input = ''
+
+#this is the while loop. while count does not equal 3 we keep repeating the loop.
+while  count != 3
+  puts "what do you want to say to grandma?"
+    input = gets.chomp
+
+    # this is where we check for upcase.
+      if input != input.upcase
+        puts "SPEAK UP, I CAN'T HEAR YOU!"
+       
+    # here we check for bye.
+      else
+        if input == "BYE" 
+          puts "I'D LIKE TO TALK A LITTLE MORE."
+        
+          # if input is BYE then it gets a count.
+          count = count +1
+        else  
+         # if it's not bye then it gives the random response.
+           puts input + " EH? NOT SINCE " + (1980 + rand(20)).to_s + "!"
+         
+           #and resets the count to zero.
+           count = 0
+        end
+       end
+       
+
+end
